5b7ae80bbed538619ce10d657d01e0249d92da44
milenoss <milenkhanal@gmail.com>
2019-11-13 14:23:37 +0000

    form

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ function startTimer(duration, display) {
         minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
+        minutes = minutes < 10? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
         display.textContent = minutes + ":" + seconds;
@@ -136,7 +136,7 @@ window.onload = function () {
 };
 
 
-/// Lives Function 
+// Lives Function 
 // might need id to connect live to the game
 
 function setLives(){
@@ -150,55 +150,35 @@ const newLive = live - 1
 //Kill function 
 function killed(){
     let kill = parseInt(document.querySelector('#kill').innerText)
-    const newKill = Kill ++ 
+    const newKill = kill ++ 
 }
 
 
 
+//form action 
+
+const form = document.querySelector('#form')
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    debugger
+const name = document.createElement('div')
+name.class = 'name'
+
+const score = document.createElement('div')
+score.class = 'score'
+
+
+})
+
+
+ 
 
 
 
 
 
 
-// timer.children.forEach(function(time)
-// {
-//     setinterval({
-//         time 
-//     },
-//     100)
-
-// })
 
 
 
 
-
-
-
-// Not needed so far
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     let elements = []
-//     let container = document.querySelector('#container')
-//     // Add each row to the array
-//     container.querySelectorAll('.row').forEach(el => elements.push(el))
-//     // Clear the container
-//     container.innerHTML = ''
-//     // Sort the array from highest to lowest
-//     elements.sort((a, b) => b.querySelector('.score').textContent - a.querySelector('.score').textContent)
-//     // Put the elements back into the container
-//     elements.forEach(e => container.appendChild(e))
-//   })
-
-//   const playerArray = [
-//     {name: "Player1", score: "430", id:"player1"},
-//     {name: "Player2", score: "580"}, id:"player2"},
-//     {name: "Player3", score: "310"}, id:"player3"},
-//     {name: "Player4", score: "640" id:"player4"},
-//     {name: "Player5", score: "495", id:"player5"}
-//   ]
-
-
-
-// 
